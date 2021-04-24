8490405dc48fed9a78b29d0904a31979b588674c
Hemang <hemang2050@gmail.com>
2021-04-24 20:35:42 +0530

    Update Title.js

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -4,8 +4,8 @@ const Title = () => {
   return (
     <div className="title">
       <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h2>Photo Gallery</h2>
+      <p>Add your best moments here.</p>
     </div>
   )
 }
