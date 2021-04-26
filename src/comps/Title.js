1c857713ff62e9333889c314e1db31d634eff401
Muneeb <muneeb.riyaz75@gmail.com>
2021-04-26 15:38:11 +0530

    Initial commit

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>FireGram</h1>
+      <h2>Photo Gallery</h2>
+      <p>Add your best moments here.</p>
+    </div>
+  )
+}
+
+export default Title;
---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyB3U7-ncCWJ6A8qV1jGK72pe7iNpsbQouk",
+  authDomain: "photo-gallery-app-54db1.firebaseapp.com",
+  projectId: "photo-gallery-app-54db1",
+  storageBucket: "photo-gallery-app-54db1.appspot.com",
+  messagingSenderId: "450287699028",
+  appId: "1:450287699028:web:18f3935ebc1623e61599c1"
+};
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const projectStorage = firebase.storage();
+  const projectFirestore = firebase.firestore();
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+  export { projectStorage, projectFirestore, timestamp };
