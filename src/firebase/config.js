280ac2976da985e395992db150bf9021025bd88e
Muneeb2017 <muneeb.riyaz75@gmail.com>
2021-08-24 19:49:30 +0530

    firebase config change

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,15 +4,16 @@ import 'firebase/firestore';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyAF6rhIFKsbAdcz8EfUeu-T26SpkJZM3Ww",
-    authDomain: "hm-firegram.firebaseapp.com",
-    projectId: "hm-firegram",
-    storageBucket: "hm-firegram.appspot.com",
-    messagingSenderId: "284716811643",
-    appId: "1:284716811643:web:17d09501d11aa595aa69de"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyB3U7-ncCWJ6A8qV1jGK72pe7iNpsbQouk",
+  authDomain: "photo-gallery-app-54db1.firebaseapp.com",
+  databaseURL: "https://photo-gallery-app-54db1-default-rtdb.firebaseio.com",
+  projectId: "photo-gallery-app-54db1",
+  storageBucket: "photo-gallery-app-54db1.appspot.com",
+  messagingSenderId: "450287699028",
+  appId: "1:450287699028:web:18f3935ebc1623e61599c1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
   const projectStorage = firebase.storage();
   const projectFirestore = firebase.firestore();
