933d134b7ad2ee731b8d3000e29b29713877bb59
Muneeb2017 <muneeb.riyaz75@gmail.com>
2021-09-10 00:22:41 +0530

    firebase property change

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,16 @@
-import  firebase from 'firebase/app';
+import   firebase from 'firebase/app';
 import 'firebase/storage';
 import 'firebase/firestore';
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyB3U7-ncCWJ6A8qV1jGK72pe7iNpsbQouk",
-  authDomain: "photo-gallery-app-54db1.firebaseapp.com",
-  databaseURL: "https://photo-gallery-app-54db1-default-rtdb.firebaseio.com",
-  projectId: "photo-gallery-app-54db1",
-  storageBucket: "photo-gallery-app-54db1.appspot.com",
-  messagingSenderId: "450287699028",
-  appId: "1:450287699028:web:18f3935ebc1623e61599c1"
+const firebaseConfig = {
+  apiKey: "AIzaSyAWm543R_3ht7kr3kGKTCO_FC5l0fhCWbE",
+  authDomain: "photo-gallery-app-3d08e.firebaseapp.com",
+  projectId: "photo-gallery-app-3d08e",
+  storageBucket: "photo-gallery-app-3d08e.appspot.com",
+  messagingSenderId: "764907014805",
+  appId: "1:764907014805:web:c777aa8e77fb1f08a1e99c",
+  measurementId: "G-XKWJWFSBZ7"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
